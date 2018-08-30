ecaef0d28655930e909b80d9680bd43a154136f3
mzwae <mrmonjaie@gmail.com>
2018-08-30 22:55:58 +0100

    added components templates

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="dashboard">
+    <h3>Dashboard</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'dashboard',
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
