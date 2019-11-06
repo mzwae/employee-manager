32289b2363dd24d04ffe789e0fb3950bd86420c4
mzwae <mrmonjaie@gmail.com>
2019-11-06 07:55:05 +0000

    updated db config vars for heroku

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: "AIzaSyD2VaI-VrNQApmq3hTQOgiquRn4ck_tFLw",
-  authDomain: "vuefs-prod-49de0.firebaseapp.com",
-  databaseURL: "https://vuefs-prod-49de0.firebaseio.com",
-  projectId: "vuefs-prod-49de0",
-  storageBucket: "vuefs-prod-49de0.appspot.com",
-  messagingSenderId: "705617015115"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId
 }
