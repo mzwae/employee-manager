06e730fb2cd4062e436ba087c8d1ac390eb5cbb1
mzwae <mrmonjaie@gmail.com>
2019-11-06 08:55:11 +0000

    Added dotenv for heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const serveStatic = require('serve-static');
 const path = require('path');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
 //Serve files from dist directory containing index.html file
 app.use('/', serveStatic(path.join(__dirname, '/dist')));
@@ -9,5 +11,6 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')));
 const port = process.env.PORT || 8081;
 
 app.listen(port);
+
 
 console.log('Listening on port: ', port);
